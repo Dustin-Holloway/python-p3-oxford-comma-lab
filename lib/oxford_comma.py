f66fa7b976d30042f80c6b472e01ffceceaab266
Dustin <dustinhollowaypdx@gmail.com>
2023-05-03 12:09:56 -0700

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,18 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        string = " ".join(items)
+        return string
+    elif len(items) == 2:
+        string = " and ".join(items)
+        return string
+    elif len(items) >= 3:
+        updated_list = items[:-1] + [f"and {items[-1]}"]
+        update = ", ".join(updated_list)
+        return update
+
+
+
+        
+
+    
+
